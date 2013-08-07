71f1d8c103a9887a8899d1fae5473a5c33c322c2
Mike <mikednepr@gmail.com>
2013-08-07 00:31:44 -0400

    Additional tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,4 +32,21 @@ describe "Static pages" do
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+  describe "Static pages" do
+    it "should have right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect( page ).to have_title( full_title( 'About Us' ) )
+      click_link "Help"
+      expect(page).to have_title( full_title( 'Help' ) ) # fill in
+      click_link "Contact"
+      expect(page).to have_title( full_title( 'Contact' ) ) # fill in
+      click_link "Home"
+      click_link "Sign up now!"
+      expect(page).to have_title( full_title( 'Sign up' ) ) # fill in
+      click_link "sample_app"
+      expect(page).to have_title( full_title( '' ) ) # fill in
+    end
+  end
 end
